034a744efb1790442060e3f5c772b8ced471d115
Gonzalo <palmer_85@hotmail.com>
2020-07-08 22:29:56 -0500

    Updates

---
+++ precache-manifest.01e9206739437d80bad17b0137edd8a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cc895ea5c4500e9ccc209a363648cc",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "dbd8ed117a27c2aa21f4",
+    "url": "/portafolio/static/css/main.90474c47.chunk.css"
+  },
+  {
+    "revision": "dedddfd5e7d98d0a0b5f",
+    "url": "/portafolio/static/js/2.9c49240d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.9c49240d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd8ed117a27c2aa21f4",
+    "url": "/portafolio/static/js/main.ced7cb50.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "489a811cff0239355b6252367f3000d8",
+    "url": "/portafolio/static/media/code-coding-computer-data-574071.489a811c.jpg"
+  }
+]);
